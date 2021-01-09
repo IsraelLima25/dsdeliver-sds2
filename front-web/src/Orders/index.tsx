35af125d89901d666104fb838f60d091b4184995
Israel <israel_lima_16@hotmail.com>
2021-01-09 12:18:47 -0300

    Add Routes and StepsHeader

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,14 @@
+import './styles.css';
+import StepsHeader from './StepsHeader';
+
+function Orders(){
+    return (
+      <div>
+          <h1 className="orders-container">
+              <StepsHeader />
+          </h1>
+      </div>
+    )
+}
+
+export default Orders;
